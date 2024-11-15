85dc3d3ee9a0e179b2b232747778b290643fa503
Linus Rachlis <linus@rachlis.net>
2024-11-14 20:09:24 -0500

    nvim: use tpope surround

--- .config/nvim/lua/plugins/surround.lua
+++ .config/nvim/lua/plugins/surround.lua
@@ -1,10 +1,4 @@
 return {
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    "tpope/vim-surround",
     event = "VeryLazy",
-    config = function()
-        require("nvim-surround").setup({
-            -- Configuration here, or leave empty to use defaults
-        })
-    end
 }
