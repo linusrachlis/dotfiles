e7be98e1bad7afa5d5445d47f8ec57e96cc9131d
Linus Rachlis <linus.rachlis@tophatmonocle.com>
2024-12-13 16:37:22 -0500

    nvim: adjust snacks keymaps

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -13,9 +13,17 @@ return {
         lazygit = { enabled = true},
     },
     keys = {
-        { "]]", function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
-        { "[[", function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
-        { "<leader>gB", function() Snacks.gitbrowse() end, desc = "Git Browse" },
+        { "<leader>]", function() Snacks.words.jump(vim.v.count1) end, desc = "Next Reference", mode = { "n", "t" } },
+        { "<leader>[", function() Snacks.words.jump(-vim.v.count1) end, desc = "Prev Reference", mode = { "n", "t" } },
+        { "<leader>go", function() Snacks.gitbrowse() end, desc = "Git Browse (Open in browser)" },
+        { "<leader>gy", function()
+            Snacks.gitbrowse({
+                open = function(url)
+                    print(url)
+                    vim.fn.setreg('+', url)
+                end
+            })
+        end, desc = "Git Yank Permalink" },
         { "<leader>gb", function() Snacks.git.blame_line() end, desc = "Git Blame Line" },
         { "<leader>gf", function() Snacks.lazygit.log_file() end, desc = "Lazygit Current File History" },
         { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
