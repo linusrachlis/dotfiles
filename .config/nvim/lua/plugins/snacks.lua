0a7d73bce46c80176ef8f4c9f0c849cc1a133cf1
Linus Rachlis <linus@rachlis.net>
2024-12-16 09:27:36 -0500

    nvim: turn off snacks.notifier

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -3,8 +3,6 @@ return {
     priority = 1000,
     lazy = false,
     opts = {
-        -- bigfile = { enabled = true },
-        notifier = { enabled = true },
         quickfile = { enabled = true },
         statuscolumn = { enabled = true },
         words = { enabled = true },
