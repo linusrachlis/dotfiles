ad29615bdf6a32ecfba85488051b06226adaf03f
Linus Rachlis <linus.rachlis@tophatmonocle.com>
2024-12-23 13:47:12 -0500

    nvim: set fzf layout to vertical

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -4,7 +4,13 @@ return {
   dependencies = {
     "nvim-tree/nvim-web-devicons",
   },
-  opts = {},
+  opts = {
+    winopts = {
+      preview = {
+        layout = 'vertical'
+      }
+    }
+  },
   keys = {
     -- FzfLua commands picker works better than Telescope command finder, I think that doesn't include command mode commands
     { "<leader>fp",  "<cmd>FzfLua commands<CR>",                   desc = 'Fzf command pallette' },
