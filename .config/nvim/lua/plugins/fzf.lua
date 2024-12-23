f63ba30c36569d60e4ece9176e5b3ffab8940614
Linus Rachlis <linus.rachlis@tophatmonocle.com>
2024-12-23 15:40:34 -0500

    nvim: use telescope for most pickers

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -1,5 +1,6 @@
 return {
   "ibhagwan/fzf-lua",
+  lazy = false,
   -- optional for icon support
   dependencies = {
     "nvim-tree/nvim-web-devicons",
@@ -14,10 +15,6 @@ return {
   keys = {
     -- FzfLua commands picker works better than Telescope command finder, I think that doesn't include command mode commands
     { "<leader>fp",  "<cmd>FzfLua commands<CR>",                   desc = 'Fzf command pallette' },
-    { "<leader>fld", "<cmd>FzfLua lsp_document_symbols<CR>",       desc = 'Fzf LSP document symbols' },
-    { "<leader>flw", "<cmd>FzfLua lsp_live_workspace_symbols<CR>", desc = 'Fzf LSP workspace symbols' },
-    { "<leader>fdd", "<cmd>FzfLua lsp_document_diagnostics<CR>",   desc = 'Fzf LSP document diagnostics' },
-    { "<leader>fdw", "<cmd>FzfLua lsp_workspace_diagnostics<CR>",  desc = 'Fzf LSP workspace diagnostics' },
     { "<leader>fzb", "<cmd>FzfLua builtin<CR>",                    desc = 'Fzf builtin pickers' },
   },
 }
