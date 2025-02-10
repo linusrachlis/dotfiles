25ffcca7f45f7842291eb15149547fc5935010b8
Linus Rachlis <linus.rachlis@tophatmonocle.com>
2025-02-10 09:47:50 -0500

    nvim: switch to everforest and remove Shade
    Shade had a bad bug when closing a tab

--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -3,30 +3,41 @@ return {
     "folke/tokyonight.nvim",
     enabled = false,
     lazy = false,
-    priority = 1000,
+    -- priority = 1000,
     -- opts = {},
   },
   {
     "catppuccin/nvim",
+    enabled = false,
     lazy = false,
     name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        background = {
-          light = "latte",
-          dark = "frappe"
-        }
-      })
-      vim.cmd.colorscheme("catppuccin")
-    end
+    -- priority = 1000,
+    -- config = function()
+    --   require("catppuccin").setup({
+    --     background = {
+    --       light = "latte",
+    --       dark = "frappe"
+    --     }
+    --   })
+    --   vim.cmd.colorscheme("catppuccin")
+    -- end
   },
   {
     "rose-pine/neovim",
     name = "rose-pine",
+    enabled = false,
     lazy = false,
     -- config = function()
     --   vim.cmd("colorscheme rose-pine")
     -- end
+  },
+  {
+    "neanias/everforest-nvim",
+    version = false,
+    lazy = false,
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      vim.cmd([[colorscheme everforest]])
+    end
   }
 }
--- .config/nvim/lua/plugins/shade.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "sunjon/Shade.nvim",
-  opts = {
-    overlay_opacity = 65,
-  },
-}
