922a99afbc3607a6537c000f77dc0b2d3eadcded
Linus Rachlis <linus.rachlis@tophatmonocle.com>
2025-02-14 14:32:35 -0500

    nvim: window nav keymaps

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -96,8 +96,8 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohls<cr>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+-- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+-- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -146,6 +146,12 @@ vim.keymap.set({'n', 'v'}, '<leader>gy', function ()
   print(full_gh_permalink)
   vim.fn.setreg('+', full_gh_permalink)
 end, { desc = 'Git Yank Permalink' })
+
+-- Easier window navigation
+vim.keymap.set('n', '<C-h>', '<C-w>h')
+vim.keymap.set('n', '<C-l>', '<C-w>l')
+vim.keymap.set('n', '<C-k>', '<C-w>k')
+vim.keymap.set('n', '<C-j>', '<C-w>j')
 
 -- Easier window resizing
 vim.keymap.set('n', '<C-=>', '<cmd>:resize +5<CR>')
