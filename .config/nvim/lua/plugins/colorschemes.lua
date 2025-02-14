30d916026d7795e7aa5bf2c46d93f40337fff18a
Linus Rachlis <linus.rachlis@tophatmonocle.com>
2025-02-14 14:33:12 -0500

    nvim: tokyo night theme

--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -1,16 +1,26 @@
 return {
   {
     "folke/tokyonight.nvim",
-    enabled = false,
+    enabled = true,
     lazy = false,
-    -- priority = 1000,
-    -- opts = {},
+    priority = 1000,
+    opts = {},
+    config = function ()
+      require("tokyonight").setup({
+        background = {
+          style = "storm",
+          light_style = "day",
+          dim_inactive = true,
+        }
+      })
+      vim.cmd.colorscheme("tokyonight")
+    end
   },
   {
     "catppuccin/nvim",
     enabled = false,
-    lazy = false,
-    name = "catppuccin",
+    -- lazy = false,
+    -- name = "catppuccin",
     -- priority = 1000,
     -- config = function()
     --   require("catppuccin").setup({
@@ -25,22 +35,23 @@ return {
   {
     "rose-pine/neovim",
     name = "rose-pine",
-    enabled = false,
-    lazy = false,
+    -- enabled = false,
+    -- lazy = false,
     -- config = function()
     --   vim.cmd("colorscheme rose-pine")
     -- end
   },
   {
     "neanias/everforest-nvim",
-    version = false,
-    lazy = false,
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      require("everforest").setup({
-        dim_inactive_windows = true,
-      })
-      vim.cmd([[colorscheme everforest]])
-    end
+    enabled = false,
+    -- version = false,
+    -- lazy = false,
+    -- priority = 1000, -- make sure to load this before all the other start plugins
+    -- config = function()
+    --   require("everforest").setup({
+    --     dim_inactive_windows = true,
+    --   })
+    --   vim.cmd([[colorscheme everforest]])
+    -- end
   }
 }
