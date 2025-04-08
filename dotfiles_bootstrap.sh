c5959fdc4b46bb79cc2c6b9567f11624fa2c7fa0
Linus Rachlis <linus.rachlis@tophat.com>
2025-04-08 19:14:11 -0400

    tmux cleanup

--- dotfiles_bootstrap.sh
+++ dotfiles_bootstrap.sh
@@ -2,22 +2,5 @@
 ## Run this after cloning or pulling dotfiles repo. It should be idempotent.
 ###
 
-##
-# Tmux - clone TPM and catppuccin theme
-##
-TMUX_TPM_PATH=~/.tmux/plugins/tpm
-if [ -d $TMUX_TPM_PATH ]; then
-    echo "TPM already installed (directory exists: $TMUX_TPM_PATH)"
-else
-    echo "Installing TPM"
-    git clone https://github.com/tmux-plugins/tpm $TMUX_TPM_PATH
-fi
-
-# This theme is breaking window names for some reason
-# TMUX_CATP_PATH=~/.config/tmux/plugins/catppuccin/tmux
-# if [ -d $TMUX_CATP_PATH ]; then
-#     echo "Tmux catppuccin already installed (directory exists: $TMUX_CATP_PATH)"
-# else
-#     echo "Installing Tmux catppuccin"
-#     git clone -b v2.1.2 https://github.com/catppuccin/tmux.git $TMUX_CATP_PATH
-# fi
+# Nothing here right now
+echo "🤷"
--- dotfiles_readme.md
+++ dotfiles_readme.md
@@ -8,7 +8,4 @@ https://mjones44.medium.com/storing-dotfiles-in-a-git-repository-53f765c0005d
 **After** cloning or pulling dotfiles, (re-)run `dotfiles_bootstrap.sh`. It
 should be idempotent.
 
-Its reason for existence so far is to install Tmux's TPM plugin, for which the
-procedure is a git clone. I don't want to mess with submodules and I don't want
-to just statically version all the TPM source files, _and_ I don't want to end
-up with various bespoke setup steps for dotfiles stuff.
+It doesn't actually do anything right now, I used to need it for Tmux setup.
