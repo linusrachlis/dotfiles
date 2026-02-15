ad31c631d9e8702d465ca2b36e769b767d28eb75
Linus Rachlis <linus.rachlis@tophat.com>
2026-02-15 10:22:24 -0500

    nvim: editable qflist

--- .config/nvim/after/ftplugin/qf.lua
+++ .config/nvim/after/ftplugin/qf.lua
@@ -1,1 +1,6 @@
-vim.bo.errorformat = [[%f\|%l\ col\ %c\|%m]]
+-- Allow editing and re-parsing qflist with :cgetbuf
+vim.opt_local.errorformat = {
+  "%f\\|%l\\ col\\ %c-%k\\|%m", -- with column range
+  "%f\\|%l\\ col\\ %c\\|%m",    -- with only column start
+  "%f\\|%l\\|%m",               -- without column
+}
