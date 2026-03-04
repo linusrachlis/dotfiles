d0e7a9e3f79b6aba8ddde93c72b41e8cb02bd563
Linus Rachlis <linus@rachlis.net>
2026-03-04 09:00:59 -0500

    nvim: catppuccin theme

--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -1,10 +1,9 @@
 return {
   {
     "folke/tokyonight.nvim",
-    enabled = true,
+    enabled = false,
     lazy = false,
     priority = 1000,
-    opts = {},
     config = function()
       require("tokyonight").setup({
         background = {
@@ -16,4 +15,22 @@ return {
       vim.cmd.colorscheme("tokyonight")
     end
   },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    lazy = false,
+    config = function()
+      require("catppuccin").setup({
+        background = {
+          light = "latte",
+          dark = "macchiato",
+        },
+        show_end_of_buffer = true,
+        auto_integrations = true,
+      })
+
+      vim.cmd.colorscheme("catppuccin")
+    end
+  }
 }
