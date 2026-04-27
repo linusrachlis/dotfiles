1fe23e2a7c05d80c8b8cd8cb0e5fb49c6b7cf843
Linus Rachlis <linus.rachlis@tophat.com>
2026-04-27 09:16:18 -0400

    nvim: buf cmds to write to make qflist always modifiable
    (Still needs work)

--- .config/nvim/after/ftplugin/qf.lua
+++ .config/nvim/after/ftplugin/qf.lua
@@ -4,3 +4,19 @@ vim.opt_local.errorformat = {
   "%f\\|%l\\ col\\ %c\\|%m",    -- with only column start
   "%f\\|%l\\|%m",               -- without column
 }
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  buffer = 0,
+  callback = function()
+    print("buf enter")
+    vim.opt_local.modifiable = true
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufWriteCmd", {
+  buffer = 0,
+  callback = function()
+    print("buf write")
+    vim.cmd("cgetbuf")
+  end,
+})
